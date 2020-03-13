7a6b7bca7945cac5724cbae3d30008217e053834
Esko-Kalervo Salaka <esko.salaka@gmail.com>
2020-03-13 09:46:29 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from mtg_api import app
-
+import os
+print(os.environ.get("DATABASE_URL"))
 
 if __name__ == "__main__":
     print(app.config)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
